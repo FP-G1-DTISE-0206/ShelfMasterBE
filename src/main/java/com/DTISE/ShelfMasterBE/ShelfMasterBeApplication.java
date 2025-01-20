2199d17b1f13928ae9649893d7945a83328cd0f1
goraasep <gora.asep@gmail.com>
2025-01-20 15:27:05 +0700

    chore: first ini

---
+++ src/main/java/com/DTISE/ShelfMasterBE/ShelfMasterBeApplication.java
@@ -0,0 +1,13 @@
+package com.DTISE.ShelfMasterBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfMasterBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfMasterBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DTISE/ShelfMasterBE/ShelfMasterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DTISE.ShelfMasterBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfMasterBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
